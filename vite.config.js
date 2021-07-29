6fd972375bd209ccb7beab43b8b2e98406f91048
Matt McCormick <matt.mccormick@kitware.com>
2021-07-29 15:19:56 -0400

    build(vite): Generate ES module

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src', 'main.jsx'),
+      name: 'materialUIMachineOptions',
+      formats: ['es'],
+      fileName: 'materialUIMachineOptions.js',
+    },
+  },
 })
