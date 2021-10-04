6acff5a5cf47542cc78a9fc721d97fd29aa6985e
Brianna Major <brianna.major@kitware.com>
2021-10-04 13:31:27 -0400

    feat(LayersInterface): Add interface for collection of layers

---
+++ src/Layers/LayersInterface.jsx
@@ -0,0 +1,23 @@
+import { useActor } from '@xstate/react'
+import React, { useEffect, useRef } from 'react'
+import LayerInterface from './LayerInterface'
+
+function LayersInterface(props) {
+  const { service } = props
+  const layersUIGroup = useRef(null)
+  const [ state ] = useActor(service)
+
+  useEffect(() => {
+    state.context.layers.uiLayers = new Map()
+    state.context.layers.layersUIGroup = layersUIGroup.current
+    state.context.uiGroups.set('layers', layersUIGroup.current)
+  }, [])
+
+  return(
+    <div className='layersUIGroup' ref={ layersUIGroup }>
+      <LayerInterface className='layersUIRow' { ...props } children={ React.Children }/>
+    </div>
+  )
+}
+
+export default LayersInterface
