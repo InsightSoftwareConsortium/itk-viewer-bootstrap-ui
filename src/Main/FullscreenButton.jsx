c2d2cf3b533b8c75829cc93e7885c41c588e39fe
Laryssa Abdala <la.abdala@gmail.com>
2022-05-25 13:48:33 -0400

    feat(bootstrap): change fullscreen button

--- src/Main/FullscreenButton.jsx
+++ src/Main/FullscreenButton.jsx
@@ -1,43 +1,32 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import { useActor } from '@xstate/react'
-import { Icon, ToggleButton, Tooltip } from '@mui/material'
 import { fullscreenIconDataUri } from 'itk-viewer-icons'
-import '../style.css'
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import cn from 'classnames'
 
 function FullscreenButton(props) {
   const { service } = props
-  const fullscreenButton = useRef(null)
   const [state, send] = useActor(service)
 
-  useEffect(() => {
-    state.context.main.fullscreenButton = fullscreenButton.current
-  }, [])
-
-  return (
-    <Tooltip
-      ref={fullscreenButton}
-      title="Fullscreen [f]"
-      PopperProps={{
-        anchorEl: fullscreenButton.current,
-        disablePortal: true,
-        keepMounted: true
-      }}
+  return ( 
+    <OverlayTrigger
+      transition={false}
+      overlay={<Tooltip> Fullscreen [f] </Tooltip>}
     >
-      <ToggleButton
-        size="small"
-        className="toggleButton"
-        value="fullscreenEnabled"
-        selected={state.context.main.fullscreenEnabled}
-        onChange={() => {
-          send('TOGGLE_FULLSCREEN')
-        }}
+      <Button
+      className={cn('icon-button', {
+        checked:state.context.main.fullscreenEnabled
+      })}
+      onClick={() => send('TOGGLE_FULLSCREEN')}
+      variant='secondary'
       >
-        <Icon>
-          <img src={fullscreenIconDataUri} />
-        </Icon>
-      </ToggleButton>
-    </Tooltip>
-  )
-}
+        <Image src={fullscreenIconDataUri}></Image>
+      </Button>
+    </OverlayTrigger>
+    )
+ }
 
 export default FullscreenButton
