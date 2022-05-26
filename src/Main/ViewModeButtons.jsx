48fd960129f0f76651190448b0937aa33f1cfb3b
Laryssa Abdala <la.abdala@gmail.com>
2022-05-26 15:31:41 -0400

    feat(bootstrap): Change view mode buttons
    Change view mode buttons (XPlane, YPlane, ZPlane, Volume) from Material UI to Bootstrap.
    Besides changing the framework, this implementation uses a list map to condense the previous version of the code.

--- src/Main/ViewModeButtons.jsx
+++ src/Main/ViewModeButtons.jsx
@@ -1,136 +1,78 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { useActor } from '@xstate/react'
-import { Icon, ToggleButton, Tooltip } from '@mui/material'
 import {
   volumeIconDataUri,
   redPlaneIconDataUri,
   yellowPlaneIconDataUri,
   greenPlaneIconDataUri
 } from 'itk-viewer-icons'
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import cn from 'classnames'
+
+function ViewButton(props) {
+  const [state, send] = useActor(props.service)
+  let dataNameLowerCase =
+    props.dataName.charAt(0).toLowerCase() + props.dataName.slice(1)
+  let enableButton = dataNameLowerCase + 'EnableButton'
+
+  return (
+    <OverlayTrigger
+      transition={false}
+      overlay={<Tooltip> {props.title} </Tooltip>}
+    >
+      <Button
+        className={cn('icon-button', {
+          checked: state.context.main[enableButton]
+        })}
+        onClick={() => {
+          send({ type: 'VIEW_MODE_CHANGED', data: props.dataName })
+        }}
+        variant="secondary"
+      >
+        <Image src={props.imageSrc}></Image>
+      </Button>
+    </OverlayTrigger>
+  )
+}
 
 function ViewModeButtons(props) {
   const { service } = props
-  const xPlaneButton = useRef(null)
-  const yPlaneButton = useRef(null)
-  const zPlaneButton = useRef(null)
-  const volumeButton = useRef(null)
-  const [state, send] = useActor(service)
 
-  useEffect(() => {
-    state.context.main.xPlaneButtonLabel = xPlaneButton.current
-    state.context.main.yPlaneButtonLabel = yPlaneButton.current
-    state.context.main.zPlaneButtonLabel = zPlaneButton.current
-    state.context.main.volumeButtonLabel = volumeButton.current
-  }, [])
-
-  return (
-    <div className="viewModeButtons">
-      <Tooltip
-        ref={xPlaneButton}
-        title="X plane [1]"
-        PopperProps={{
-          anchorEl: xPlaneButton.current,
-          disablePortal: true,
-          keepMounted: true
-        }}
-      >
-        <ToggleButton
-          size="small"
-          className="toggleButton"
-          value="xplane"
-          selected={state.context.main.xPlaneButton}
-          onChange={() => {
-            send({ type: 'VIEW_MODE_CHANGED', data: 'XPlane' })
-          }}
-        >
-          <Icon className="viewModeButton">
-            <img
-              style={{ height: '24px', width: '24px' }}
-              src={redPlaneIconDataUri}
-            />
-          </Icon>
-        </ToggleButton>
-      </Tooltip>
-      <Tooltip
-        ref={yPlaneButton}
-        title="Y plane [2]"
-        PopperProps={{
-          anchorEl: yPlaneButton.current,
-          disablePortal: true,
-          keepMounted: true
-        }}
-      >
-        <ToggleButton
-          size="small"
-          className="toggleButton"
-          value="yplane"
-          selected={state.context.main.yPlaneButton}
-          onClick={() => {
-            send({ type: 'VIEW_MODE_CHANGED', data: 'YPlane' })
-          }}
-        >
-          <Icon className="viewModeButton">
-            <img
-              style={{ height: '24px', width: '24px' }}
-              src={yellowPlaneIconDataUri}
-            />
-          </Icon>
-        </ToggleButton>
-      </Tooltip>
-      <Tooltip
-        ref={zPlaneButton}
-        title="z plane [3]"
-        PopperProps={{
-          anchorEl: zPlaneButton.current,
-          disablePortal: true,
-          keepMounted: true
-        }}
-      >
-        <ToggleButton
-          size="small"
-          className="toggleButton"
-          value="zplane"
-          selected={state.context.main.zPlaneButton}
-          onClick={() => {
-            send({ type: 'VIEW_MODE_CHANGED', data: 'ZPlane' })
-          }}
-        >
-          <Icon className="viewModeButton">
-            <img
-              style={{ height: '24px', width: '24px' }}
-              src={greenPlaneIconDataUri}
-            />
-          </Icon>
-        </ToggleButton>
-      </Tooltip>
-      <Tooltip
-        ref={volumeButton}
-        title="Volume [4]"
-        PopperProps={{
-          anchorEl: volumeButton.current,
-          disablePortal: true,
-          keepMounted: true
-        }}
-      >
-        <ToggleButton
-          size="small"
-          className="toggleButton"
-          value="volume"
-          selected={state.context.main.volumeButton}
-          onClick={() => {
-            send({ type: 'VIEW_MODE_CHANGED', data: 'Volume' })
-          }}
-        >
-          <Icon className="viewModeButton">
-            <img
-              style={{ height: '24px', width: '24px' }}
-              src={volumeIconDataUri}
-            />
-          </Icon>
-        </ToggleButton>
-      </Tooltip>
-    </div>
-  )
+  const buttonList = [
+    {
+      title: 'X plane [1]',
+      dataName: 'XPlane',
+      imageSrc: redPlaneIconDataUri
+    },
+    {
+      title: 'Y plane [2]',
+      dataName: 'YPlane',
+      imageSrc: yellowPlaneIconDataUri
+    },
+    {
+      title: 'Z plane [3]',
+      dataName: 'ZPlane',
+      imageSrc: greenPlaneIconDataUri
+    },
+    {
+      title: 'Volume [4]',
+      dataName: 'Volume',
+      imageSrc: volumeIconDataUri
+    }
+  ]
+  const listItems = buttonList.map(({ title, dataName, imageSrc }) => (
+    <ViewButton
+      key={title}
+      title={title}
+      dataName={dataName}
+      imageSrc={imageSrc}
+      service={service}
+    ></ViewButton>
+  ))
+  return listItems
 }
 
 export default ViewModeButtons
