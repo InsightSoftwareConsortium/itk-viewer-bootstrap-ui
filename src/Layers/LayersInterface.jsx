8f21b32de7328ab42b353ab44b83a7f441cf80f5
Laryssa Abdala <la.abdala@gmail.com>
2022-06-08 09:38:48 -0400

    feat(LayersInterface): Delete useActor

--- src/Layers/LayersInterface.jsx
+++ src/Layers/LayersInterface.jsx
@@ -1,4 +1,4 @@
-import { useActor, useSelector } from '@xstate/react'
+import { useSelector } from '@xstate/react'
 import React, { useEffect, useRef } from 'react'
 import LayerInterface from './LayerInterface'
 
