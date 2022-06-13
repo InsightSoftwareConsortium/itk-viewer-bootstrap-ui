71a09714fa261930a947932386a57c5c75d432f2
Laryssa Abdala <la.abdala@gmail.com>
2022-06-13 09:38:00 -0400

    feat(Style/.icon-image): Remove gray background from image icons
    Use one rule for image icons with a permanent transparent background.

--- src/Images/SampleDistanceSlider.jsx
+++ src/Images/SampleDistanceSlider.jsx
@@ -49,7 +49,7 @@ function SampleDistanceSlider(props) {
         transition={false}
         overlay={<Tooltip>Volume sample distance</Tooltip>}
       >
-        <div className="icon-button icon-image" ref={spacingDiv}>
+        <div className="icon-image" ref={spacingDiv}>
           <Image src={sampleDistanceIconDataUri}></Image>
         </div>
       </OverlayTrigger>
