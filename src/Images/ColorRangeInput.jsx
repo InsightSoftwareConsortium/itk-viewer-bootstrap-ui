3dd7de0393349039d6a1872f41ff7f5c5a5986e8
Laryssa Abdala <la.abdala@gmail.com>
2022-06-17 12:19:49 -0400

    feat(ColorRangeInput): Translate from MUI to bootstrap
     We use OverlayTrigger instead a title for the `Min` and `Max` labels.
    Another possibility is to use Card and Card.Header.

--- src/Images/ColorRangeInput.jsx
+++ src/Images/ColorRangeInput.jsx
@@ -1,25 +1,54 @@
 import React, { useEffect, useRef } from 'react'
-import { useActor } from '@xstate/react'
-import { Icon, TextField, ToggleButton, Tooltip } from '@mui/material'
+import { useSelector } from '@xstate/react'
 import { interpolationIconDataUri } from 'itk-viewer-icons'
 import ColorMapIconSelector from './ColorMapIconSelector'
 import '../style.css'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Image from 'react-bootstrap/Image'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import cn from 'classnames'
+import { ConstructionOutlined } from '@mui/icons-material'
 
 function ColorRangeInput(props) {
   const { service } = props
   const colorRangeInput = useRef(null)
   const interpolationButton = useRef(null)
-  const [state, send] = useActor(service)
-  const name = state.context.images.selectedName
-  const actorContext = state.context.images.actorContext.get(name)
+  const send = service.send
+  const name = useSelector(
+    service,
+    (state) => state.context.images.selectedName
+  )
+  const actorContext = useSelector(service, (state) =>
+    state.context.images.actorContext.get(state.context.images.selectedName)
+  )
+  const interpolationEnabled = useSelector(
+    service,
+    (state) =>
+      state.context.images.actorContext.get(state.context.images.selectedName)
+        .interpolationEnabled
+  )
+  const colorRanges = useSelector(
+    service,
+    (state) =>
+      state.context.images.actorContext.get(state.context.images.selectedName)
+        .colorRanges
+  )
+
+  const colorRangesSelected = useSelector(service, (state) =>
+    state.context.images.actorContext
+      .get(state.context.images.selectedName)
+      .colorRanges.get(actorContext.selectedComponent)
+  )
 
   useEffect(() => {
-    state.context.images.colorRangeInputRow = colorRangeInput.current
+    service.machine.context.images.colorRangeInputRow = colorRangeInput.current
   }, [colorRangeInput.current])
 
   const interpolate = () => {
     if (actorContext) {
-      return actorContext.interpolationEnabled
+      return interpolationEnabled
     }
     return true
   }
@@ -27,15 +56,19 @@ function ColorRangeInput(props) {
   const toggleInterpolate = () => {
     send({
       type: 'TOGGLE_IMAGE_INTERPOLATION',
-      data: state.context.images.selectedName
+      data: name
     })
+  }
+
+  if (!actorContext) {
+    console.log(actorContext)
   }
 
   const currentRange = () => {
     let range = [0, 0]
     if (actorContext) {
-      if (actorContext.colorRanges.size) {
-        range = actorContext.colorRanges.get(actorContext.selectedComponent)
+      if (colorRanges.size) {
+        range = colorRangesSelected
       }
     }
     return range
@@ -77,61 +110,54 @@ function ColorRangeInput(props) {
     rangeChanged(minVal, parseFloat(val))
   }
 
-  return actorContext && actorContext.colorRanges.size ? (
-    <div
-      ref={colorRangeInput}
-      className="uiRow"
-      style={{ background: 'rgba(127, 127, 127, 0.5)' }}
-    >
-      <Tooltip
-        ref={interpolationButton}
-        title="Interpolation"
-        PopperProps={{
-          anchorEl: interpolationButton.current,
-          disablePortal: true,
-          keepMounted: true
-        }}
+  return (
+    actorContext &&
+    colorRanges.size && (
+      <div
+        ref={colorRangeInput}
+        className="uiRow"
+        style={{ background: 'rgba(127, 127, 127, 0.5)' }}
       >
-        <ToggleButton
-          size="small"
-          className="interpolationButton toggleButton"
-          value="interpolation"
-          selected={interpolate()}
-          onChange={() => {
-            toggleInterpolate()
-          }}
+        <OverlayTrigger
+          transition={false}
+          overlay={<Tooltip>Interpolation</Tooltip>}
         >
-          <Icon>
-            <img src={interpolationIconDataUri} />
-          </Icon>
-        </ToggleButton>
-      </Tooltip>
-      <TextField
-        className="numberInput"
-        type="number"
-        label="Min"
-        variant="outlined"
-        size="small"
-        value={currentRangeMin()}
-        onChange={(e) => {
-          rangeMinChanged(e.target.value)
-        }}
-      />
-      <ColorMapIconSelector {...props} />
-      <TextField
-        className="numberInput"
-        type="number"
-        label="Max"
-        variant="outlined"
-        size="small"
-        value={currentRangeMax()}
-        onChange={(e) => {
-          rangeMaxChanged(e.target.value)
-        }}
-      />
-    </div>
-  ) : (
-    <div />
+          <Button
+            className={cn('icon-button', {
+              checked: interpolate()
+            })}
+            onClick={() => {
+              toggleInterpolate()
+            }}
+            variant="secondary"
+            ref={interpolationButton}
+          >
+            <Image src={interpolationIconDataUri}></Image>
+          </Button>
+        </OverlayTrigger>
+        <OverlayTrigger transition={false} overlay={<Tooltip>Min</Tooltip>}>
+          <Form.Control
+            className="numberInput"
+            type="number"
+            value={currentRangeMin()}
+            onChange={(e) => {
+              rangeMinChanged(e.target.value)
+            }}
+          />
+        </OverlayTrigger>
+        <ColorMapIconSelector {...props} />
+        <OverlayTrigger transition={false} overlay={<Tooltip>Max</Tooltip>}>
+          <Form.Control
+            className="numberInput"
+            type="number"
+            value={currentRangeMax()}
+            onChange={(e) => {
+              rangeMaxChanged(e.target.value)
+            }}
+          />
+        </OverlayTrigger>
+      </div>
+    )
   )
 }
 
