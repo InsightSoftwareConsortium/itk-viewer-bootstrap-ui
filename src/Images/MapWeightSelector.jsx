ff91286d161633894f0d06433730a2f9aaf2333d
Paul Elliott <paul.elliott@kitware.com>
2022-06-21 16:55:45 -0400

    fix(labelmap weights): avoid controled-uncontrolled warning, refactor

--- src/Images/MapWeightSelector.jsx
+++ src/Images/MapWeightSelector.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useSelector } from '@xstate/react'
 import Form from 'react-bootstrap/Form'
 import '../style.css'
 
-function MapWeightSelector(props) {
-  const { service } = props
+const ALL_LABELS = { name: 'All', value: 'all' }
+
+function MapWeightSelector({ service }) {
   const send = service.send
-  let labelMapWeights = [{ name: 'All', value: 'all' }]
   const name = useSelector(
     service,
     (state) => state.context.images.selectedName
@@ -17,21 +17,26 @@ function MapWeightSelector(props) {
       state.context.images.actorContext.get(state.context.images.selectedName)
         .labelNames
   )
+  const selectedLabel = useSelector(
+    service,
+    (state) =>
+      state.context.images.actorContext.get(state.context.images.selectedName)
+        .selectedLabel
+  )
 
-  labelNames.forEach((value, key) => {
-    labelMapWeights.push({
-      name: value,
-      value: key
-    })
-  })
+  const labelsAsOptions = Array.from(labelNames).map(([key, name]) => ({
+    name,
+    value: key
+  }))
 
-  const [weightValue, setWeightValue] = useState(labelMapWeights[0].name)
+  const labelOptions = [ALL_LABELS, ...labelsAsOptions]
 
   const handleChange = (event) => {
-    setWeightValue(event.target.value)
+    const value = event.target.value
+    const selectedLabel = value === 'all' ? value : Number(value)
     send({
       type: 'LABEL_IMAGE_SELECTED_LABEL_CHANGED',
-      data: { name, selectedLabel: weightValue }
+      data: { name, selectedLabel }
     })
   }
 
@@ -40,12 +45,12 @@ function MapWeightSelector(props) {
       <div className="uiRow">
         <Form.Control
           as="select"
-          value={weightValue}
+          value={selectedLabel}
           onChange={(e) => {
             handleChange(e)
           }}
         >
-          {labelMapWeights.map((weight, idx) => (
+          {labelOptions.map((weight, idx) => (
             <option key={idx} value={weight.value}>
               {weight.name}
             </option>
