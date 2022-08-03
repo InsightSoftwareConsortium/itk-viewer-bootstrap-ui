InsightSoftwareConsortium/itk-viewer-bootstrap-ui

commit 2505e4634662f86f76377d3edb525441c76e9350

fix(ColorRangeInput): Improve logic for step

src/Images/ColorRangeInput.jsx
@@ -72,7 +72,12 @@ function ColorRangeInput(props) {
   const currentRangeMin = currentRange[0]
   const currentRangeMax = currentRange[1]
 
-  const [step] = useState((currentRange[1] - currentRange[0]) / 200)
+  const [rangeMin, rangeMax] =
+    actorContext.image.scaleInfo[actorContext.renderedScale].ranges[
+      actorContext.selectedComponent
+    ]
+  const step =
+    imageType.slice(0, 5) === 'float' ? (rangeMax - rangeMin) / 200 : 1
   const [minIntent, setminIntent] = useState(currentRangeMin)
   const [maxIntent, setmaxIntent] = useState(currentRangeMax)
 
@@ -168,7 +173,7 @@ function ColorRangeInput(props) {
             onChange={(e) => {
               rangeMinChanged(e.target.value)
             }}
-            step={imageType.slice(0, 5) === 'float' ? step : 1}
+            step={step}
           />
         </OverlayTrigger>
         <ColorMapIconSelector {...props} />
@@ -182,7 +187,7 @@ function ColorRangeInput(props) {
             onChange={(e) => {
               rangeMaxChanged(e.target.value)
             }}
-            step={imageType.slice(0, 5) === 'float' ? step : 1}
+            step={step}
           />
         </OverlayTrigger>
       </div>

commit 7b70b8ecc1d59123d5075a53bd89158c582d3d2c

fix(ColorRangeInput): Support 2d images with various color channels
Fixes bug introduced in 2505e4634662f86f76377d3edb525441c76e9350.

src/Images/ColorRangeInput.jsx
@@ -72,10 +72,11 @@ function ColorRangeInput(props) {
   const currentRangeMin = currentRange[0]
   const currentRangeMax = currentRange[1]
 
-  const [rangeMin, rangeMax] =
-    actorContext.image.scaleInfo[actorContext.renderedScale].ranges[
-      actorContext.selectedComponent
-    ]
+  const [rangeMin, rangeMax] = actorContext.selectedComponent
+    ? actorContext.image.scaleInfo[actorContext.renderedScale].ranges[
+        actorContext.selectedComponent
+      ]
+    : [0, 0]
   const step =
     imageType.slice(0, 5) === 'float' ? (rangeMax - rangeMin) / 200 : 1
   const [minIntent, setminIntent] = useState(currentRangeMin)