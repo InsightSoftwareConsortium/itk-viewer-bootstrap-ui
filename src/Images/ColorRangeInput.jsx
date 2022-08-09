InsightSoftwareConsortium/itk-viewer-bootstrap-ui

commit 2505e4634662f86f76377d3edb525441c76e9350

fix(ColorRangeInput): Improve logic for step

src/Images/ColorRangeInput.jsx
@@ -72,7 +72,12 @@ function ColorRangeInput(props) {
   const currentRangeMin = currentRange[0]
   const currentRangeMax = currentRange[1]
 
-  const [step] = useState((currentRange[1] - currentRange[0]) / 200)
+  const [rangeMin, rangeMax] =
+    actorContext.image.scaleInfo[actorContext.renderedScale].ranges[
+      actorContext.selectedComponent
+    ]
+  const step =
+    imageType.slice(0, 5) === 'float' ? (rangeMax - rangeMin) / 200 : 1
   const [minIntent, setminIntent] = useState(currentRangeMin)
   const [maxIntent, setmaxIntent] = useState(currentRangeMax)
 
@@ -168,7 +173,7 @@ function ColorRangeInput(props) {
             onChange={(e) => {
               rangeMinChanged(e.target.value)
             }}
-            step={imageType.slice(0, 5) === 'float' ? step : 1}
+            step={step}
           />
         </OverlayTrigger>
         <ColorMapIconSelector {...props} />
@@ -182,7 +187,7 @@ function ColorRangeInput(props) {
             onChange={(e) => {
               rangeMaxChanged(e.target.value)
             }}
-            step={imageType.slice(0, 5) === 'float' ? step : 1}
+            step={step}
           />
         </OverlayTrigger>
       </div>

commit 7bb8fb407cad874f31e569f2159226575eb6436d

fix(ColorRangeInput): Support images with multiple color channels (#80)
- Fixes bug introduced in 2505e4634662f86f76377d3edb525441c76e9350.
- Fixes error when color channels 2 and 3 get selected
Notice that for an image with components, `actorContext.image.scaleInfo[actorContext.renderedScale].ranges[actorContext.selectedComponent]` is undefined and we have to capture the channel color range using `actorContext.colorRangeBounds.get(actorContext.selectedComponent)`

 -  Apply colormap to the transfer function editor on startup

Co-authored-by: Paul Elliott <paul.elliott@kitware.com>

 - Change conditions of useEffect for multiple components

This extra condition ensures that the colormap will change as we select different color channels.

src/Images/ColorRangeInput.jsx
@@ -72,10 +72,24 @@ function ColorRangeInput(props) {
   const currentRangeMin = currentRange[0]
   const currentRangeMax = currentRange[1]
 
-  const [rangeMin, rangeMax] =
-    actorContext.image.scaleInfo[actorContext.renderedScale].ranges[
+  let [rangeMin, rangeMax] = [0, 0]
+
+  if (
+    actorContext.componentVisibilities.length === 1 &&
+    actorContext.selectedComponent
+  ) {
+    ;[rangeMin, rangeMax] =
+      actorContext.image.scaleInfo[actorContext.renderedScale].ranges[
+        actorContext.selectedComponent
+      ]
+  } else if (
+    actorContext.componentVisibilities.length > 1 &&
+    actorContext.colorRangeBounds.get(actorContext.selectedComponent)
+  ) {
+    ;[rangeMin, rangeMax] = actorContext.colorRangeBounds.get(
       actorContext.selectedComponent
-    ]
+    )
+  }
   const step =
     imageType.slice(0, 5) === 'float' ? (rangeMax - rangeMin) / 200 : 1
   const [minIntent, setminIntent] = useState(currentRangeMin)

src/Images/TransferFunctionWidget.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState, useCallback } from 'react'
 import { useActor } from '@xstate/react'
 
 import vtkMouseRangeManipulator from '@kitware/vtk.js/Interaction/Manipulators/MouseRangeManipulator'
@@ -50,9 +50,6 @@ function TransferFunctionWidget(props) {
       })
       transferFunctionWidget.setEnableRangeZoom(true)
       let iconSize = 20
-      if (state.context.use2D) {
-        iconSize = 0
-      }
       transferFunctionWidget.updateStyle({
         backgroundColor: 'rgba(255, 255, 255, 0.2)',
         histogramColor: 'rgba(30, 30, 30, 0.6)',
@@ -205,14 +202,18 @@ function TransferFunctionWidget(props) {
 
   useEffect(() => {
     const lut = state.context.images.lookupTableProxies
-    if (lut && lut.size === 1) {
+    if (lut) {
       const componentIndex = actorContext.selectedComponent
       send({
         type: 'IMAGE_COLOR_MAP_CHANGED',
-        data: { name, component: componentIndex, colorMap: 'Grayscale' }
+        data: {
+          name,
+          component: componentIndex,
+          colorMap: actorContext.colorMaps.get(actorContext.selectedComponent)
+        }
       })
     }
-  }, [state.context.images.lookupTableProxies])
+  }, [actorContext.selectedComponent, state.context.images.lookupTableProxies])
 
   // Manage update when opacity changes
   const onAnimationChange = (start) => {