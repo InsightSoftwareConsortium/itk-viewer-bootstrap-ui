b08b6a79d84ce3416ce84026bcea02a4901bebdf
Paul Elliott <paul.elliott@kitware.com>
2022-09-14 14:49:54 -0400

    fix(TransferFunctionWidget): useSelector instead of apply actions

--- src/Images/TransferFunctionWidget.jsx
+++ src/Images/TransferFunctionWidget.jsx
@@ -2,7 +2,7 @@ import { useSelector } from '@xstate/react'
 import React, { useEffect, useRef } from 'react'
 
 import '../style.css'
-import getSelectedImageContext from './getSelectedImageContext'
+import { getSelectedImageContext } from './getSelectedImageContext'
 import { setup } from './transferFunctionWidgetUtils'
 
 const selectLookupTable = (state) => {
@@ -13,6 +13,27 @@ const selectLookupTable = (state) => {
 }
 
 const lookupProxyCompare = ([, oldName], [, newName]) => oldName === newName
+
+const selectColorRangeNormalized = (state) => {
+  const { selectedComponent, colorRanges, colorRangeBounds } =
+    getSelectedImageContext(state)
+  const colorRange = colorRanges.get(selectedComponent) ?? [0, 1]
+  const fullRange = colorRangeBounds.get(selectedComponent) ?? colorRange
+  const diff = fullRange[1] - fullRange[0]
+  const colorRangeNormalized = [
+    (colorRange[0] - fullRange[0]) / diff,
+    (colorRange[1] - fullRange[0]) / diff
+  ]
+  return colorRangeNormalized
+}
+
+const areArraysEqual = (a, b) =>
+  a.length === b.length && a.every((aValue, index) => aValue === b[index])
+
+const selectHistogram = (state) => {
+  const { selectedComponent, histograms } = getSelectedImageContext(state)
+  return histograms.get(selectedComponent)
+}
 
 function TransferFunctionWidget({ service }) {
   const transferFunctionWidgetContainer = useRef(null)
@@ -39,6 +60,20 @@ function TransferFunctionWidget({ service }) {
       )
     }
   }, [transferFunctionWidget, presetNameToTriggerUpdate, lookupTable])
+
+  const colorRangeNormalized = useSelector(
+    service,
+    selectColorRangeNormalized,
+    areArraysEqual
+  )
+  useEffect(() => {
+    transferFunctionWidget.current.setRangeZoom(colorRangeNormalized)
+  }, [colorRangeNormalized, transferFunctionWidget])
+
+  const histogram = useSelector(service, selectHistogram)
+  useEffect(() => {
+    if (histogram) transferFunctionWidget.current.setHistogram(histogram)
+  }, [histogram, transferFunctionWidget])
 
   return (
     <div className="uiRow" style={{ background: 'rgba(127, 127, 127, 0.5)' }}>
--- src/Images/selectImageComponent.js
+++ src/Images/selectImageComponent.js
@@ -1,23 +1,9 @@
-import applyColorRange from './applyColorRange'
-import applyHistogram from './applyHistogram'
-
 function selectImageComponent(context, event) {
   const name = event.data.name
   const actorContext = context.images.actorContext.get(name)
   const component = event.data.component
 
   const transferFunctionWidget = context.images.transferFunctionWidget
-
-  if (actorContext.colorRanges.has(component)) {
-    const range = actorContext.colorRanges.get(component)
-    applyColorRange(context, {
-      data: {
-        name,
-        component,
-        range
-      }
-    })
-  }
 
   const piecewiseFunctionPoints =
     actorContext.piecewiseFunctionPoints.get(component)
@@ -26,15 +12,7 @@ function selectImageComponent(context, event) {
   }
 
   const histogram = actorContext.histograms.get(component)
-  if (histogram) {
-    applyHistogram(context, {
-      data: {
-        name,
-        component,
-        histogram
-      }
-    })
-  } else {
+  if (!histogram) {
     context.service.send({
       type: 'UPDATE_IMAGE_HISTOGRAM',
       data: { name, component }
