67bdcd7da87af6be65ba5c940a1ad4078afb02bd
Paul Elliott <paul.elliott@kitware.com>
2022-09-19 19:18:36 -0400

    feat(MainInterface): add cropping plane buttons

---
+++ src/Main/CroppingPlanesReset.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { resetCropIconDataUri } from 'itk-viewer-icons'
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import cn from 'classnames'
+
+function CroppingPlanesReset({ service: { send } }) {
+  return (
+    <OverlayTrigger
+      transition={false}
+      overlay={<Tooltip>Reset Cropping Planes</Tooltip>}
+    >
+      <Button
+        className={cn('icon-button')}
+        onClick={() => {
+          send('RESET_CROPPING_PLANES')
+          send('CROPPING_PLANES_CHANGED_BY_USER')
+        }}
+        variant="secondary"
+      >
+        <Image src={resetCropIconDataUri}></Image>
+      </Button>
+    </OverlayTrigger>
+  )
+}
+
+export default CroppingPlanesReset
